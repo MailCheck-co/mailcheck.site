f3c90a9ec34f089ede02d8a0060834f3f846f852
dnesterenko27 <d.nesterenko27@gmail.com>
2018-02-05 19:39:10 +0200

    Started animation.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,31 @@
-var mySwiper = new Swiper ('.swiper-container', {
-  slidesPerView: 1,
+const form = document.getElementById('contact-us');
+const popUp = document.getElementById('popup-itself');
+const popUpBlock = document.getElementById('popup-block');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    popUp.classList.add("open");
+    popUpBlock.classList.add("open");
+
+    document.body.classList.add('fixed');
+
+    popUpBlock.addEventListener("click", function (event) {
+        let target = event.target;
+        if (target.classList.contains('popup-close') || target.classList.contains("popup-container")) {
+            popUpBlock.classList.remove('open');
+            document.body.classList.remove('fixed');
+        }
+    })
+});
+
+
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
     initialSlide: 0,
     loop: true,
-  spaceBetween: 50
+    spaceBetween: 50
 });
 
