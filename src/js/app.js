094ae75606bd03560953ba052474775286991ed7
dnesterenko27 <d.nesterenko27@gmail.com>
2018-02-06 17:53:56 +0200

    Changed mobile menu. Fixed some issues

--- src/js/app.js
+++ src/js/app.js
@@ -12,16 +12,47 @@ form.addEventListener('submit', function (e) {
         let target = event.target;
         if (target.classList.contains('popup-close') || target.classList.contains("popup-container")) {
             popUpBlock.classList.add('close');
-            setTimeout( function() {
-                popUpBlock.classList.remove('open');
-            }, 2000 );
+            setTimeout(function () {
+                popUpBlock.classList.remove('open', 'close');
+            }, 1000);
             document.body.classList.remove('fixed');
         }
     })
 });
 
 
+const burger = document.getElementById('burger');
+const mobileMenu = document.getElementById('mobile-menu');
 
+burger.addEventListener('click', function () {
+    if (!burger.classList.contains('open')) {
+        burger.classList.add('open');
+        document.body.classList.add('fixed');
+        mobileMenu.classList.add('open');
+        mobileMenu.addEventListener('click',  function (event) {
+            let target = event.target;
+            if (target.classList.contains('nav-link') || target.classList.contains('mobile-btn')) {
+                mobileMenu.classList.add('close');
+                burger.classList.remove('open');
+                setTimeout(function () {
+                    mobileMenu.classList.remove('open');
+                    document.body.classList.remove('fixed');
+                    mobileMenu.classList.remove('open');
+                    mobileMenu.classList.remove('close');
+                }, 1000);
+            }
+        });
+    } else {
+        mobileMenu.classList.add('close');
+        burger.classList.remove('open');
+        setTimeout(function () {
+            mobileMenu.classList.remove('open');
+            document.body.classList.remove('fixed');
+            mobileMenu.classList.remove('open');
+            mobileMenu.classList.remove('close');
+        }, 1000);
+    }
+});
 
 
 var mySwiper = new Swiper('.swiper-container', {
