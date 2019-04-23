88b00daf7942bee242d76d88206b7a4d1a2c2ebe
i.rohozhyn <i.rohozhyn@nodeart.io>
2019-04-23 11:11:59 +0300

    Article update:
    * 3 articles was added;
    * h3 on blog page was replaced with a tag

--- src/rightApproach.js
+++
@@ -1,3 +0,0 @@
-import 'normalize.css/normalize.css';
-import './scss/styles.scss';
-import './js/refer';
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,13 +9,16 @@ module.exports = {
     terms: './src/terms.js',
     privacy: './src/privacy.js',
     blog: './src/blog.js',
-    csvFile: './src/csv-file.js',
-    rightApproach: './src/rightApproach.js',
-    increaseEmailsDeliverability: './src/increaseEmailsDeliverability.js',
-    bounceRate: './src/bounceRate.js',
-    catchAllServers: './src/catchAllServers.js',
-    guide: './src/guide.js',
-    scholarship: './src/scholarship.js',
+    csvFile: './src/blog.js',
+    rightApproach: './src/blog.js',
+    increaseEmailsDeliverability: './src/blog.js',
+    bounceRate: './src/blog.js',
+    catchAllServers: './src/blog.js',
+    guide: './src/blog.js',
+    scholarship: './src/blog.js',
+    googlePlus: './src/blog.js',
+    emailMarketingTips: './src/blog.js',
+    sellingCartCustomization: './src/blog.js',
     refer: './src/refer.js',
     404: './src/404.js',
   },
@@ -152,6 +155,24 @@ module.exports = {
       template: './src/html/blog/scholarship.hbs',
       filename: 'scholarship.html',
       chunks: ['scholarship'],
+      inject: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/html/blog/googlePlus.hbs',
+      filename: 'googlePlus.html',
+      chunks: ['googlePlus'],
+      inject: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/html/blog/emailMarketingTips.hbs',
+      filename: 'emailMarketingTips.html',
+      chunks: ['emailMarketingTips'],
+      inject: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/html/blog/sellingCartCustomization.hbs',
+      filename: 'sellingCartCustomization.html',
+      chunks: ['sellingCartCustomization'],
       inject: true
     }),
     new HtmlWebpackPlugin({
