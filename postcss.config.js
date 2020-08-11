d3637b41e169624477547f87cd19c1ffe6c8576d
Nosov Konstantin <nosov@nodeart.io>
2020-08-11 03:14:12 +0300

    fix browserslist warning

--- postcss.config.js
+++ postcss.config.js
@@ -9,12 +9,6 @@ module.exports = {
         * Adds vendor prefixes to css attributes
         * https://github.com/postcss/autoprefixer
         */
-        require('autoprefixer')({
-            /* It should add vendor prefixes for the last 2 versions of all browsers, meaning old prefixes such as
-            * -webkit-border-radius: 5px; that the latest browsers support as border-radius won't be added.
-            * https://github.com/ai/browserslist#queries
-            */
-            browsers: 'last 2 versions'
-        })
+        require('autoprefixer')
     ]
 };
