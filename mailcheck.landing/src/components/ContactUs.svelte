8e8d8dff1f4cfeea28a09af738f7140ce88a8a1b
Vasiliy Manilo <v.manilo@nodeart.io>
2021-01-06 16:47:28 +0200

    add popup component, fix faq page

--- mailcheck.landing/src/components/ContactUs.svelte
+++ mailcheck.landing/src/components/ContactUs.svelte
@@ -1,7 +1,10 @@
+<script>
+    import Popup from "./Popup.svelte";
+</script>
+
 <style lang="scss">
     @import "../scss/utilities/index";
     @import "../scss/molecules/contact-us";
-    @import "../scss/molecules/popup";
 </style>
 
 <section id="contact-us-section" class="contact-us">
@@ -19,18 +22,4 @@
     </div>
 </section>
 
-<div class="popup-container" id="popup-block">
-    <div class="popup" id="popup-success">
-        <span class="popup-close success" id="close-success" />
-        <span class="popup-thanks">Thanks for filling out our form!</span>
-        <p class="popup-text">
-            We will look over your message and get back to you by tomorrow. Your
-            friends at MailCheck!
-        </p>
-    </div>
-    <div class="popup" id="popup-error">
-        <span class="popup-close error" id="close-error" />
-        <span class="popup-thanks">Something went wrong!</span>
-        <p class="popup-text">Please try again later</p>
-    </div>
-</div>
+<Popup />
