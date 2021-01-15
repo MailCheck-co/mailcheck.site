82773701513f2201c46f7e99ac8c34119cb619a1
zakhar <z.tkachuk@nodeart.io>
2021-01-15 18:39:54 +0200

    fix: popup

--- src/components/ContactUs.svelte
+++ src/components/ContactUs.svelte
@@ -1,73 +1,55 @@
 <script>
-    import { onMount } from "svelte";
-
     let isOpen = false;
     let isError = false;
+    let contactForm, popUpBlock;
+    const onClose = () => {
+        isOpen = false;
+        isError = false;
+        document.body.classList.remove("fixed");
+    }
+    const onSubmit = e => {
+        e.preventDefault();
+        const nameValue = contactForm.querySelector("input[type=text]").value;
+        const mailValue = contactForm.querySelector("input[type=email]").value;
+        const textareaValue = contactForm.querySelector(".input-message").value;
+        const referrerValue = document.referrer;
+        const data = {
+            name: nameValue,
+            email: mailValue,
+            subject: textareaValue,
+            referrer: referrerValue,
+        };
 
-    onMount(() => {
-        const contactForm = document.getElementById("contact-us");
-        const popUpBlock = document.getElementById("popup-block");
+        const formData = new FormData();
 
-        contactForm.addEventListener("submit", function (e) {
-            const nameValue = contactForm.querySelector("input[type=text]")
-                .value;
-            const mailValue = contactForm.querySelector("input[type=email]")
-                .value;
-            const textareaValue = contactForm.querySelector(".input-message")
-                .value;
-            const referrerValue = document.referrer;
+        for (let name in data) {
+            formData.append(name, data[name]);
+        }
 
-            e.preventDefault();
-
-            const data = {
-                name: nameValue,
-                email: mailValue,
-                subject: textareaValue,
-                referrer: referrerValue,
-            };
-
-            const formData = new FormData();
-
-            for (let name in data) {
-                formData.append(name, data[name]);
-            }
-
-            fetch("/sendMail", {
-                method: "POST",
-                body: JSON.stringify(data),
+        fetch("/sendMail", {
+            method: "POST",
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.text())
+            .then(() => {
+                isOpen = true;
+                contactForm.reset();
             })
-                .then((res) => res.text())
-                .then(() => {
-                    isOpen = true;
-                    contactForm.reset();
-                })
-                .catch((e) => {
-                    document.querySelector("popup-text").textContent = e;
-                    isOpen = true;
-                    isError = true;
-                });
-
-            document.body.classList.add("fixed");
-            window.dataLayer.push({
-                eventCategory: "site",
-                eventAction: "contactform",
-                eventLabel: "submit",
-                eventValue: "",
-                event: "gaEvent",
+            .catch((e) => {
+                document.querySelector("popup-text").textContent = e;
+                isOpen = true;
+                isError = true;
             });
 
-            popUpBlock.addEventListener("click", function (event) {
-                const target = event.target;
-                if (
-                    target.classList.contains("popup-close") ||
-                    target.classList.contains("popup-container")
-                ) {
-                    isOpen = false;
-                    document.body.classList.remove("fixed");
-                }
-            });
+        document.body.classList.add("fixed");
+        window.dataLayer.push({
+            eventCategory: "site",
+            eventAction: "contactform",
+            eventLabel: "submit",
+            eventValue: "",
+            event: "gaEvent",
         });
-    });
+    }
 </script>
 
 <style lang="scss">
@@ -78,7 +60,7 @@
 
 <section id="contact-us-section" class="contact-us">
     <div class="container">
-        <form id="contact-us" class="contact-form">
+        <form class="contact-form" bind:this={contactForm} on:submit={onSubmit}>
             <h2 class="title title-contact">contact us</h2>
             <input class="input" type="text" placeholder="Name" required />
             <input class="input" type="email" placeholder="Email" required />
@@ -91,17 +73,22 @@
     </div>
 </section>
 
-<div class="popup-container" id="popup-block" class:open={isOpen}>
-    <div class="popup" id="popup-success" class:open={isOpen}>
-        <span class="popup-close success" id="close-success" />
+<div
+    class="popup-container"
+    class:open={isOpen}
+    on:click={onClose}
+    bind:this={popUpBlock}
+>
+    <div class="popup" class:open={isOpen}>
+        <span class="popup-close success" />
         <span class="popup-thanks">Thanks for filling out our form!</span>
         <p class="popup-text">
             We will look over your message and get back to you by tomorrow. Your
             friends at MailCheck!
         </p>
     </div>
-    <div class="popup" id="popup-error" class:open={isError && isOpen}>
-        <span class="popup-close error" id="close-error" />
+    <div class="popup" class:open={isError && isOpen}>
+        <span class="popup-close error" />
         <span class="popup-thanks">Something went wrong!</span>
         <p class="popup-text">Please try again later</p>
     </div>
