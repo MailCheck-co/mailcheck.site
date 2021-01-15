7e4598edd88206a2d6f76112d00884c205171eda
zakhar <z.tkachuk@nodeart.io>
2021-01-15 17:15:27 +0200

    fix: blog article

--- src/layouts/blog.svelte
+++ src/layouts/blog.svelte
@@ -1,3 +1,24 @@
-<div class="xxx">
-    <slot />
+<style>
+    .container {
+        display: block;
+        box-sizing: border-box;
+        width: 100%;
+        min-width: 320px;
+        max-width: 1200px;
+        margin: 0 auto;
+        padding: 0 30px;
+    }
+
+    .content-block {
+        display: flex;
+        flex-direction: column;
+        max-width: 880px;
+        margin: 0 auto 100px;
+    }
+</style>
+
+<div class="container">
+    <div class="content-block">
+        <slot />
+    </div>
 </div>
