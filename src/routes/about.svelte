47ed366ff6d8ff20951d32c782f980dcf5d370c5
Nosov Konstantin <nosov@nodeart.io>
2021-01-30 21:17:20 +0200

    remove unused file

--- src/routes/about.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="typescript">
-    import Seo from "../components/seo.svelte";
-</script>
-
-<Seo title="About" />
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
