85c90dbd2ab16fc98dff423bf37d4effde01395d
Farukh Muhammadiev <fm@aimbrella.com>
2021-02-05 17:12:41 +0000

    Update src/site-data.js

--- src/site-data.js
+++ src/site-data.js
@@ -4,5 +4,5 @@ export default {
   github: "https://github.com/MailCheck-co",
   twitter: "https://twitter.com/mailcheckco",
   email: "support@mailcheck.co",
-  desc: "An application to clean your subscription list from bounces, and enrich data about customers",
+  desc: "An application to clean your subscription list from bounces, and enrich data with customers",
 }
