490fb35b0cd7b40029b0a96919083e0c30435640
zakhar <z.tkachuk@nodeart.io>
2021-02-22 11:22:01 +0200

    fix: faq links

--- src/routes/faq-appsumo.svelte
+++ src/routes/faq-appsumo.svelte
@@ -25,7 +25,7 @@
                     <div class="tab-content">
                         <p class="tab-text">
                             Mailcheck provides the highest amount of the email validations AppSumo ever had, with 100,000 credits monthly limit.
-                            Currently, we allow Sumolings to verify 1 200,000  credits lifetime.
+                            Currently, we allow Sumolings to verify 2M credits lifetime.
                         </p>
                     </div>
                 </div>
@@ -41,9 +41,21 @@
                         <p class="tab-text">
                             We have several solutions,
 
-                            <br /><br />1) If you are using WordPress, you can install our plugin https://wordpress.org/plugins/mailcheck-co/ and set it up on your website, we're constantly updating our code, so you can fully rely on us.
+                            <br />
+                            <br />
+                            1) If you are using WordPress, you can <a 
+                                target="_blank" 
+                                href="https://wordpress.org/plugins/mailcheck-co/">
+                                install our plugin
+                            </a> and set it up on your website, we're constantly updating our code, so you can fully rely on us.
 
-                            <br /><br />2)Use our API to simply copy and paste within the website https://app.mailcheck.co/docs#tag--email
+                            <br />
+                            <br />
+                            2)Use our API to simply copy and paste <a 
+                                target="_blank" 
+                                href="https://app.mailcheck.co/docs#tag--email">
+                                within the website</a>
+                            
                         </p>
                     </div>
                 </div>
@@ -57,7 +69,11 @@
                     <label class="tab-label" for="radio3">I've bought several codes, how can I use them at a time?</label>
                     <div class="tab-content">
                         <p class="tab-text">
-                            In case you have more than 1 code, please go to settings -> click on "AppSumo" https://app.mailcheck.co/dashboard/appsumo here you can activate up to 5 codes.
+                            In case you have more than 1 code, please go to settings -> <a 
+                                target="_blank" 
+                                href="https://app.mailcheck.co/dashboard/appsumo">
+                                click on "AppSumo"
+                            </a> here you can activate up to 5 codes.
                         </p>
                     </div>
                 </div>
@@ -81,26 +97,13 @@
                     <input
                         class="input-radio"
                         type="checkbox"
-                        id="radio5"
-                        name="acc-tabs" />
-                    <label class="tab-label" for="radio5">Can I re-sell the code?</label>
-                    <div class="tab-content">
-                        <p class="tab-text">
-                            If you feel one code is enough for the usage, you can share the other one with your friends, please note, there is no way to use the same promo code twice.
-                        </p>
-                    </div>
-                </div>
-
-                <div class="tab">
-                    <input
-                        class="input-radio"
-                        type="checkbox"
                         id="radio6"
                         name="acc-tabs" />
                     <label class="tab-label" for="radio6">I've chosen mistakenly one of your active plans, can you switch me back to AppSumo promo?</label>
                     <div class="tab-content">
                         <p class="tab-text">
-                            Please note, in order to proceed through the additional security checks and switch back to the previous plan you need to contact security@mailcheck.co
+                            Please note, in order to proceed through the additional security checks and switch back to the previous plan you need to contact 
+                            <a href="mailto:security@mailcheck.co">security@mailcheck.co</a>
                         </p>
                     </div>
                 </div>
