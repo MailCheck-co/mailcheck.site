dfc8f9c4b78944f75ebc45df5df33cb0f2117bb4
a.katrusha@nodeart.io <a.katrusha@nodeart.io>
2021-02-23 17:38:45 +0200

    add linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,40 +4,40 @@ const path = require("path");
 const svelteConfigPath = path.resolve("./svelte.config");
 
 module.exports = {
-  env: {
-    node: true,
-    browser: true,
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    createDefaultProgram: true,
-    ecmaVersion: 2020,
-    sourceType: "module",
-  },
+	env: {
+		node: true,
+		browser: true,
+	},
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		createDefaultProgram: true,
+		ecmaVersion: 2020,
+		sourceType: "module",
+	},
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  plugins: ["svelte3", "@typescript-eslint", "prettier"],
-  overrides: [
-    {
-      files: ["*.svelte"],
-      processor: "svelte3/svelte3",
-    },
-    {
-      files: ["*.ts", "*.json"],
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-      ],
-    },
-  ],
+	plugins: ["svelte3", "@typescript-eslint", "prettier"],
+	overrides: [
+		{
+			files: ["*.svelte"],
+			processor: "svelte3/svelte3",
+		},
+		{
+			files: ["*.ts", "*.json"],
+			extends: [
+        "plugin:@typescript-eslint/recommended", 
+			],
+		},
+	],
   rules: {
     "import/first": 0,
     "import/no-duplicates": 0,
     "import/no-mutable-exports": 0,
     "import/no-unresolved": 0,
-    "@typescript-eslint/no-var-requires": 0, // fix on require method
+		'@typescript-eslint/no-var-requires': 0, // fix on require method
   },
-  settings: {
-    "svelte3/preprocess": eslintSveltePreprocess(svelteConfigPath),
-    //"svelte3/preprocess": eslintSveltePreprocess(),
-    'svelte3/ignore-styles': false
-  },
+	settings: {
+		"svelte3/preprocess": eslintSveltePreprocess(svelteConfigPath),
+		//"svelte3/preprocess": eslintSveltePreprocess(),
+		'svelte3/ignore-styles': false
+	},
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 module.exports = {
-  "tabWidth": 2,
   "svelteSortOrder": "options-styles-scripts-markup",
   "svelteStrictMode": true,
   "svelteBracketNewLine": false,
