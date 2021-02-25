1cef49170cf747fb33edd036acf9026cae79b7cb
a.katrusha@nodeart.io <a.katrusha@nodeart.io>
2021-02-25 11:55:32 +0200

    try to fix mailchimp-integration

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-	export let status: number;
-	export let error: Error;
+    export let status: number;
+    export let error: Error;
     import Seo from "../components/seo.svelte";
 
-	const dev = process.env.NODE_ENV === 'development';
+    const dev = process.env.NODE_ENV === "development";
 </script>
-
 
 <Seo title="{status}" />
 <svelte:head>
-    <meta http-equiv="refresh" content="5;https://mailcheck.co/">
+    <!-- <meta http-equiv="refresh" content="5;https://mailcheck.co/"> -->
 </svelte:head>
 <main class="terms" id="terms">
     <div class="container">
         <div class="wrapper-choose">
             <h1 class="title">Error: {status}</h1>
             <p class="text-thin art">{error.message}</p>
-            <p class="text-thin art">You will be redirected to main site in 10 seconds</p>
+            <p class="text-thin art">
+                You will be redirected to main site in 10 seconds
+            </p>
         </div>
     </div>
 </main>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+    <pre>{error.stack}</pre>
 {/if}
