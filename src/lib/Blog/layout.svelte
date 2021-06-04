cbc6dda4c102c030ab2a9f15afa0829cd1074653
tkachuk <z.tkachuk@nodeart.io>
2021-06-04 16:03:58 +0300

    fix: linted

--- src/lib/Blog/layout.svelte
+++ src/lib/Blog/layout.svelte
@@ -1,37 +1,36 @@
-<style>
-    .container {
-      display: block;
-      box-sizing: border-box;
-      width: 100%;
-      min-width: 320px;
-      max-width: 1200px;
-      margin: 0 auto;
-      padding: 0 30px;
-    }
-  
-    .content-block {
-      display: flex;
-      flex-direction: column;
-      max-width: 880px;
-      margin: 0 auto;
-    }
-  </style>
-  
-  <script lang="ts">
-    import Seo from "../components/Seo.svelte";
-    import ContactUs from "../components/ContactUs.svelte";
-  
-    export let title = "";
-    export let thumbnail = "";
-  </script>
-  
-  <Seo title={`Blog | ${title}`} desc={title} {thumbnail} isPost={true} />
-  
-  <div class="container" id="blog">
-    <div class="content-block">
-      <slot />
-    </div>
+<script lang="ts">
+  import Seo from '../components/Seo.svelte';
+  import ContactUs from '../components/ContactUs.svelte';
+
+  export let title = '';
+  export let thumbnail = '';
+</script>
+
+<Seo title={`Blog | ${title}`} desc={title} {thumbnail} isPost={true} />
+
+<div class="container" id="blog">
+  <div class="content-block">
+    <slot />
   </div>
-  
-  <ContactUs />
-  
+</div>
+
+<ContactUs />
+
+<style>
+  .container {
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    min-width: 320px;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 30px;
+  }
+
+  .content-block {
+    display: flex;
+    flex-direction: column;
+    max-width: 880px;
+    margin: 0 auto;
+  }
+</style>
