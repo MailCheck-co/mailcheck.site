fa37a945c08afd214b38b05904eeea1696c9f6a2
nosovk <nosovk@gmail.com>
2021-06-13 22:41:50 +0300

    remove unused code from head

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,44 +3,5 @@
   import '../app.scss';
 </script>
 
-<Header>
-  <main>
-    <slot />
-  </main>
-
-  <footer>
-    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-  </footer>
-
-  <style>
-    main {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      padding: 1rem;
-      width: 100%;
-      max-width: 64rem;
-      margin: 0 auto;
-      box-sizing: border-box;
-    }
-
-    footer {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: var(--size-40);
-    }
-
-    footer a {
-      font-weight: var(--weight-700);
-    }
-
-    @media (min-width: 480px) {
-      footer {
-        padding: var(--size-40) 0;
-      }
-    }
-  </style>
-</Header>
+<Header />k
 <slot />
