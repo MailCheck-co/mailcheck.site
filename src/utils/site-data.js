801fd0ffa99cef072599b7cc115d6bfe34fe7cad
nosovk <nosovk@gmail.com>
2021-07-02 00:17:51 +0300

    some small fix

--- src/utils/site-data.js
+++ src/utils/site-data.js
@@ -1,11 +1,10 @@
 export default {
   siteName: 'Mailcheck',
-  siteUrl: 'www.mailcheck.co',
+  siteUrl: 'https://www.mailcheck.co',
   github: 'https://github.com/MailCheck-co',
   twitter: 'https://twitter.com/mailcheckco',
   email: 'support@mailcheck.co',
-  desc:
-    'An application to clean your subscription list from bounces, and enrich data with customers'
+  desc: 'An application to clean your subscription list from bounces, and enrich data with customers'
 };
 
 export const inviewOptions = {
