14844c3b795d863b282afd11ecd853583a8ebc8e
vasiliy manilo <vasiliy.m@devcoz.com>
2021-07-16 16:22:18 +0300

    move some styles

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,4 +1,4 @@
-<script lang="ts" context="module">
+<script lang='ts' context='module'>
   /**
    * @type {import('@sveltejs/kit').Load}
    */
@@ -12,7 +12,7 @@
   }
 </script>
 
-<script lang="ts">
+<script lang='ts'>
   import ContactUs from '$lib/ContactUs/index.svelte';
   import Seo from '$lib/Seo/index.svelte';
   import { websiteSchema, articleSchema } from '$utils/json-ld';
@@ -23,18 +23,18 @@
 
 <Seo {desc} title={desc} schemas={[websiteSchema, articleSchema]} />
 
-<main class="terms" id="blog">
-  <div class="container">
-    <div class="content-block">
-      <h1 class="title">{desc}</h1>
+<main class='terms' id='blog'>
+  <div class='container'>
+    <div class='content-block'>
+      <h1 class='title'>{desc}</h1>
 
       {#each posts as { slug, title, snippet, date }}
-        <a class="article-title" href="blog/{slug}">{title}</a>
-        <p class="article-date">Date: {date}</p>
-        <p class="article-snippet">
+        <a class='article-title' href='blog/{slug}'>{title}</a>
+        <p class='article-date'>Date: {date}</p>
+        <p class='article-snippet'>
           {snippet}
-          <a sveltekit:prefetch class="text-thin text-thin-link" href="blog/{slug}"
-            >[Read more...]</a>
+          <a sveltekit:prefetch class='text-thin text-thin-link' href='blog/{slug}'
+          >[Read more...]</a>
         </p>
       {/each}
     </div>
@@ -43,23 +43,53 @@
 
 <ContactUs />
 
-<style lang="scss">
+<style lang='scss'>
   .terms {
     position: relative;
     z-index: 1;
   }
 
-  .article-title {
-    color: var(--primary-white);
-
-    &:hover {
+  .article {
+    &-title {
+      width: 100%;
+      height: auto;
+      margin: var(--size-20) auto 0;
       color: var(--primary-white);
-      text-decoration: underline;
-    }
-  }
+      font-weight: var(--weight-500);
+      font-size: var(--size-24);
+      line-height: 30px;
+      letter-spacing: 1.6px;
+      text-align: left;
+      text-transform: uppercase;
 
-  .article-snippet {
-    text-indent: initial;
+      &:hover {
+        color: var(--primary-white);
+        text-decoration: underline;
+      }
+    }
+
+    &-date {
+      width: 100%;
+      margin: 0 0 10px;
+      color: var(--primary-white);
+      font-weight: var(--weight-300);
+      font-size: var(--size-14);
+      line-height: var(--size-20);
+      letter-spacing: var(--letter-spacing);
+      text-align: left;
+      opacity: 0.5;
+    }
+
+    &-snippet {
+      margin: 0 0 15px;
+      color: var(--primary-white);
+      font-weight: var(--weight-300);
+      font-size: var(--size-16);
+      line-height: 24px;
+      letter-spacing: var(--letter-spacing);
+      text-align: left;
+      text-indent: initial;
+    }
   }
 
   .text-thin-link {
