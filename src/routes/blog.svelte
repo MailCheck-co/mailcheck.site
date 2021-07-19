2957c9f18b90dd694063be4dcbd05b1403628622
Vasiliy Manilo <v.manilo@nodeart.io>
2021-07-19 14:17:02 +0300

    lint styles and format

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,4 +1,4 @@
-<script lang='ts' context='module'>
+<script lang="ts" context="module">
   /**
    * @type {import('@sveltejs/kit').Load}
    */
@@ -12,7 +12,7 @@
   }
 </script>
 
-<script lang='ts'>
+<script lang="ts">
   import ContactUs from '$lib/ContactUs/index.svelte';
   import Seo from '$lib/Seo/index.svelte';
   import { websiteSchema, articleSchema } from '$utils/json-ld';
@@ -23,18 +23,18 @@
 
 <Seo {desc} title={desc} schemas={[websiteSchema, articleSchema]} />
 
-<main class='terms' id='blog'>
-  <div class='container'>
-    <div class='content-block'>
-      <h1 class='title'>{desc}</h1>
+<main class="terms" id="blog">
+  <div class="container">
+    <div class="content-block">
+      <h1 class="title">{desc}</h1>
 
       {#each posts as { slug, title, snippet, date }}
-        <a class='article-title' href='blog/{slug}'>{title}</a>
-        <p class='article-date'>Date: {date}</p>
-        <p class='article-snippet'>
+        <a class="article-title" href="blog/{slug}">{title}</a>
+        <p class="article-date">Date: {date}</p>
+        <p class="article-snippet">
           {snippet}
-          <a sveltekit:prefetch class='text-thin text-thin-link' href='blog/{slug}'
-          >[Read more...]</a>
+          <a sveltekit:prefetch class="text-thin text-thin-link" href="blog/{slug}"
+            >[Read more...]</a>
         </p>
       {/each}
     </div>
@@ -43,7 +43,7 @@
 
 <ContactUs />
 
-<style lang='scss'>
+<style lang="scss">
   .terms {
     position: relative;
     z-index: 1;
