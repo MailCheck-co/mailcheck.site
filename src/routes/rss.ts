f450c54abf2de0ac8638a17d9e4632c2abf80619
tkachuk <z.tkachuk@nodeart.io>
2021-07-19 19:05:15 +0300

    feat: rss!

--- src/routes/rss.ts
+++ src/routes/rss.ts
@@ -1,38 +1,36 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async () => {
-  const data = await fetch('/blog.json');
-  const posts = await data.json();
-  const body = render(posts.data);
-
+export const get: RequestHandler = async ({ host }) => {
+  const maxAge: number = 600;
+  const requestedPosts = await fetch(`http://${host}/blog.json`).then((res) => res.json());
+  const body = render(requestedPosts);
   const headers = {
-    'Cache-Control': `max-age=0, s-max-age=${600}`,
-    'Content-Type': 'application/rss+xml'
+    'Cache-Control': `max-age=0, s-max-age=${maxAge}`,
+    'Content-Type': 'application/xml',
   };
   return {
-    body,
-    headers
-  };
+    headers,
+    body
+  }
 };
 
-const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
+const render = (posts: any[]) => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <atom:link href="http://wwww.programmingtil.com/rss" rel="self" type="application/rss+xml" />
-    <title>ProgrammingTIL</title>
-    <link>https://www.programmingtil.com</link>
-    <description>Programming tutorials</description>
+    <atom:link href="https://www.mailcheck.co/rss" rel="self" type="application/rss+xml" />
+    <title>Mailcheck | Blog</title>
+    <link>https://www.mailcheck.co/</link>
+    <description>An application to clean your subscription list from bounces, and enrich data with customers. Verify your real customers in one way</description>
     ${posts
       .map(
         (post) => `<item>
-          <guid>https://www.programmingtil.com/posts/${post.attributes.slug}</guid>
-          <title>${post.attributes.title}</title>
-          <link>https://www.programmingtil.com/posts/${post.attributes.slug}</link>
-          <pubDate>${new Date(post.attributes.publishedAt).toUTCString()}</pubDate>
-          <description>${post.attributes.content}</description>
+          <guid>https://www.mailcheck.co/blog/${post.slug}</guid>
+          <title>${post.title}</title>
+          <link>https://www.mailcheck.co/blog/${post.slug}</link>
+          <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+          <description>${post.desc}</description>
           </item>`
       )
       .join('')}
   </channel>
-</rss>
-`;
+</rss>`;
