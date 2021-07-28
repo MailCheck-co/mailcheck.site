ca07dec4b3bad03bf76fcc4b2769e99a2768d82f
Im-Hul <imhul@users.noreply.github.com>
2021-07-28 16:41:33 +0300

    fix: sitemap generator (#377)
    * fix: sitemap generator
    
    * clean up
    
    * sitemap without noindexed pages
    
    Co-authored-by: tkachuk <z.tkachuk@nodeart.io>

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -8,17 +8,25 @@ const getUrl = (url) => {
   return `${pkg.url}${trimmed}`;
 };
 
+const filterRegexp = new RegExp('noindex');
+
 async function createSitemap() {
   const sitemap = create({ version: '1.0' }).ele('urlset', {
     xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9'
   });
 
   const pages = await fg(['build/**/*.html']);
+  
+  const filteredPages = pages.map(path => {
+    return [path, fs.readFileSync(path).toString()];
+  }).filter(([path, content]) => {
+    return !filterRegexp.test(content);
+  }).map(([path]) => { return path });
 
-  pages.forEach((page) => {
-    const url = sitemap.ele('url');
-    url.ele('loc').txt(getUrl(page));
-    url.ele('changefreq').txt('weekly');
+  filteredPages.forEach((page) => {
+      const url = sitemap.ele('url');
+      url.ele('loc').txt(getUrl(page));
+      url.ele('changefreq').txt('weekly');
   });
 
   const xml = sitemap.end({ prettyPrint: true });
