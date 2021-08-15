85527885d528e4c1f93f3840e865cc4934e0e207
vasiliy manilo <vasiliy.m@devcoz.com>
2021-08-15 22:15:40 +0300

    use color vars, edit selectors

--- src/lib/Video/index.svelte
+++ src/lib/Video/index.svelte
@@ -8,18 +8,18 @@
   };
 </script>
 
-<div class="video" on:click={showIframe}>
+<div class="video" on:click|once={showIframe}>
   {#if iframePlaceholder}
     <picture>
       <source srcset="https://i.ytimg.com/vi_webp/{id}/maxresdefault.webp" type="image/webp" />
-      <img class="video__media" src="https://i.ytimg.com/vi/{id}/maxresdefault.jpg" alt={title} />
+      <img src="https://i.ytimg.com/vi/{id}/maxresdefault.jpg" alt={title} />
     </picture>
-    <button class="video__button">
+    <button>
       <svg width="68" height="48" viewBox="0 0 68 48">
         <path
-          class="video__button-shape"
+          class="shape"
           d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" />
-        <path class="video__button-icon" d="M 45,24 27,14 27,34" />
+        <path class="icon" d="M 45,24 27,14 27,34" />
       </svg>
     </button>
   {:else}
@@ -44,7 +44,7 @@
     cursor: pointer;
   }
 
-  .video__media {
+  img {
     position: absolute;
     top: 0;
     left: 0;
@@ -53,45 +53,45 @@
     border: none;
   }
 
-  .video__button {
+  button {
     position: absolute;
     top: 50%;
     left: 50%;
     z-index: 1;
-    padding: 0;
     width: 68px;
     height: 48px;
+    padding: 0;
+    background-color: var(--transparent);
     border: none;
-    background-color: transparent;
     transform: translate(-50%, -50%);
     cursor: pointer;
   }
 
-  .video__button-shape {
-    fill: #212121;
+  .shape {
+    fill: var(--dark-06);
     fill-opacity: 0.8;
   }
 
-  .video__button-icon {
+  .icon {
     fill: var(--primary-white);
   }
 
-  .video__button:focus {
+  button:focus {
     outline: none;
   }
 
-  .video:hover .video__button-shape,
-  .video__button:focus .video__button-shape {
-    fill: #ff0000;
+  .video:hover .shape,
+  button:focus .shape {
+    fill: var(--youtube-red);
     fill-opacity: 1;
   }
 
   iframe {
     position: absolute;
     top: 0;
-    left: 0;
     right: 0;
     bottom: 0;
+    left: 0;
     width: 100%;
     height: 100%;
   }
