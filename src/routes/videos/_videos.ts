2e92b7be08721b5030217d9e0a73ae4d247467d8
Nosov <nosov@nodeart.io>
2021-08-20 23:17:37 +0300

    better log

--- src/routes/videos/_videos.ts
+++ src/routes/videos/_videos.ts
@@ -2,11 +2,10 @@ const fetchData = async () => {
   const apiKey = import.meta.env.VITE_YOUTUBE_API_KEY;
   const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?playlistId=PLI3Dd_Z__sqcUmD8MaIFMIVnfEvTtyPZg&part=snippet&key=${apiKey}`;
   const headers = { accept: 'application/json' };
-
   try {
     return fetch(url, { headers }).then((response) => response.json());
   } catch (error) {
-    console.error('Error:' + error);
+    console.error('Error fetching youtube: ' + error);
   }
 };
 
