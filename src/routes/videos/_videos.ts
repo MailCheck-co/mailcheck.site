eca01f1870d08f4e75a66d92ddc7e2ed4b38dd2b
tkachuk <z.tkachuk@nodeart.io>
2021-08-25 12:18:10 +0300

    _videos.ts update

--- src/routes/videos/_videos.ts
+++ src/routes/videos/_videos.ts
@@ -1,12 +1,11 @@
 const fetchData = async () => {
   const apiKey = import.meta.env.VITE_YOUTUBE_API_KEY;
-  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?playlistId=PLI3Dd_Z__sqcUmD8MaIFMIVnfEvTtyPZg&part=snippet&key=${apiKey}`;
+  const playlistId = 'PLI3Dd_Z__sqcUmD8MaIFMIVnfEvTtyPZg';
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?playlistId=${playlistId}&part=snippet&key=${apiKey}`;
   const headers = { accept: 'application/json' };
-  try {
-    return fetch(url, { headers }).then((response) => response.json());
-  } catch (error) {
-    console.error('Error fetching youtube: ' + error);
-  }
+  return fetch(url, { headers })
+      .then((response) => response.json())
+      .catch((error) => new Error('Error fetching youtube: ' + error));
 };
 
 export default fetchData;
