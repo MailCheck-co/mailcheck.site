0c9b2705597bcc4beb0995f05745b2f6c3ca7a4a
Vasiliy Manilo <v.manilo@nodeart.io>
2021-09-30 20:45:41 +0300

    add affil section image

--- src/lib/Affiliate/index.svelte
+++ src/lib/Affiliate/index.svelte
@@ -1,6 +1,7 @@
 <script lang='ts'>
   import { inview } from 'svelte-inview';
   import { inviewOptions } from '$utils/site-data';
+  import imgAffiliate from './invite-friends.jpg';
 
   let intersecting: boolean;
 </script>
@@ -19,14 +20,23 @@
   </div>
 
   <div class='container'>
-    <p class='section-description'>
-      Invite your friends and provide them with a $10 USD discount, and receive your part ($10 USD
-      as well) the number of invitations unlimited, stack validations forever! Invite 3 friends in a
-      row and grab the "Enterprise plan" (20k validations) fully free.
-    </p>
-    <a rel='external' href='https://app.mailcheck.co/auth/login' title='Invite now' target='_blank'>
-      Invite now
-    </a>
+    <div class='affil-block'>
+      <div class='affil-block-inner'>
+        <div class='affil-block-image'>
+          <img src={imgAffiliate} class='affil-image' alt='Invite friends'>
+        </div>
+        <div class='affil-block-content'>
+          <p class='section-description'>
+            Invite your friends and provide them with a $10 USD discount, and receive your part ($10 USD
+            as well) the number of invitations unlimited, stack validations forever! Invite 3 friends in a
+            row and grab the "Enterprise plan" (20k validations) fully free.
+          </p>
+          <a class='affil-btn' rel='external' href='https://app.mailcheck.co/auth/login' title='Invite now' target='_blank'>
+            Invite now
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 
@@ -80,12 +90,32 @@
       margin: 0 0 var(--size-40);
       font-weight: var(--weight-300);
       letter-spacing: var(--letter-spacing);
-      text-indent: 0;
       color: var(--primary-white);
-      text-align: center;
+      text-indent: 0;
     }
 
-    a {
+    .affil-block {
+      padding: var(--size-20) 0;
+    }
+
+    .affil-block-image {
+      max-width: 50%;
+    }
+
+    .affil-block-inner {
+      display: flex;
+      align-items: stretch;
+      gap: var(--size-40);
+    }
+
+    .affil-block-content {
+      padding: var(--size-30) 0;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .affil-btn {
+      margin-top: auto;
       border: var(--size-1) solid var(--primary-white);
       border-radius: var(--size-6);
       color: var(--primary-white);
@@ -95,7 +125,6 @@
       height: var(--size-60);
       letter-spacing: var(--letter-spacing);
       line-height: var(--size-60);
-      margin: 0 auto;
       max-width: 10rem;
       outline: none;
       padding: 0 var(--size-40);
@@ -114,13 +143,29 @@
         margin-bottom: var(--size-60);
       }
     }
-  }
 
-  @media (min-width: 728px) {
-    .section-description {
-      font-size: var(--size-18);
-      line-height: var(--size-28);
-      text-align: center;
+    @media (min-width: 728px) {
+      .section-description {
+        font-size: var(--size-18);
+        line-height: var(--size-28);
+      }
+    }
+
+    @media (max-width: 1024px) {
+
+      .affil-block-inner {
+        flex-direction: column;
+        text-align: center;
+        gap: 0;
+      }
+      .affil-block-image {
+        max-width: 100%;
+        margin: 0 auto;
+      }
+
+      .affil-btn {
+        margin: 0 auto;
+      }
     }
   }
 </style>
