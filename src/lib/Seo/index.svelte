86cea40d01eaf5fe99d3ad53318920e2cd8427ef
mefistofelus <manilo@ukr.net>
2022-01-13 18:47:45 +0200

    update deps, add fixes according to framework changes

--- src/lib/Seo/index.svelte
+++ src/lib/Seo/index.svelte
@@ -19,18 +19,20 @@
 
 <svelte:head>
   <title>{siteName} | {title}</title>
-  <link rel="canonical" href={canonical ? siteUrl + canonical : siteUrl + ($page.path ?? '')} />
+  <link
+    rel="canonical"
+    href={canonical ? siteUrl + canonical : siteUrl + ($page.url.pathname ?? '')} />
   <meta name="description" content={desc || defaultDesc} />
   <meta name="robots" content={noindex ? 'noindex' : 'all'} />
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content={isPost ? 'blog' : 'website'} />
-  <meta property="og:url" content="{siteUrl}{$page.path ?? ''}" />
+  <meta property="og:url" content="{siteUrl}{$page.url.pathname ?? ''}" />
   <meta property="og:title" content={title || siteName} />
   <meta property="og:description" content={desc || defaultDesc} />
   <meta property="og:image" content={thumbnail || siteUrl + '/favicon.png'} />
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="{siteUrl}{$page.path ?? ''}" />
+  <meta property="twitter:url" content="{siteUrl}{$page.url.pathname ?? ''}" />
   <meta property="twitter:title" content={title || siteName} />
   <meta property="twitter:description" content={desc || defaultDesc} />
   {#if thumbnail !== ''}<meta property="twitter:image" content={thumbnail} />{/if}
