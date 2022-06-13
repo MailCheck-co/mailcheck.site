af0ba4f42914d45e046e66d15834f11aa085cb78
Im-Hul <imhul@users.noreply.github.com>
2022-06-13 12:41:43 +0300

    fix: fonts (#1091)
    @imhul помоему это не все, https://github.com/sw-yx/spark-joy#font-loading-strategy
    посмотри по ссылке описание проблем\решений
    
    Co-authored-by: Zakhar Tkachuk <z.tkachuk@nodeart.io>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,8 @@
   import Header from '$lib/Header/index.svelte';
   import Footer from '$lib/Footer/index.svelte';
   import '../app.scss';
+  import "@fontsource/fira-mono";
+  import "@fontsource/roboto";
 </script>
 
 <Header />
