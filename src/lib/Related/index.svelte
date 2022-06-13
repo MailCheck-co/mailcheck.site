1acfff99b94acc3297b8805dbaa624df3fbce541
Zakhar Tkachuk <z.tkachuk@nodeart.io>
2022-06-13 15:40:35 +0300

    feat: related news component

---
+++ src/lib/Related/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let posts = [];
+  </script>
+  
+  <div class="related">
+    {#each posts as { slug, title, snippet, thumbnail }}
+      <div class="post">
+        <img src={thumbnail} alt={title} />
+        <h1 class="uppercase">{title}</h1>
+        <p>{snippet}</p>
+        <a href="/blog/{slug}" class="btn uppercase">read now</a>
+      </div>
+    {/each}
+  </div>
+  
+  <style lang="scss">
+    .related {
+      display: flex;
+      flex-direction: column;
+  
+      @media only screen and (min-width: 768px) {
+        flex-direction: row;
+      }
+  
+      .post {
+        width: 30%;
+        flex: 1 0 30%;
+  
+        img {
+          width: 100%;
+          height: auto;
+        }
+      }
+    }
+  </style>
