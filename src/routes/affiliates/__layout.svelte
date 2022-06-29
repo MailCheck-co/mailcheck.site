18006f3a14353a85e0aba177a3f84931662b8889
Zakhar Tkachuk <z.tkachuk@nodeart.io>
2022-06-29 12:38:20 +0300

    fix: affiliates

--- src/routes/affiliates/__layout.svelte
+++ src/routes/affiliates/__layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
   import ContactUs from '$lib/ContactUs/index.svelte';
+  import Seo from '$lib/Seo/index.svelte';
+  import { websiteSchema } from '$utils/json-ld';
+
+  const title = 'Mailcheck affiliate program';
+  const desc = title + '. Earn the highest commissions on referrals.';
 </script>
+
+<Seo {desc} {title} schemas={[websiteSchema]} />
 
 <slot />
 
