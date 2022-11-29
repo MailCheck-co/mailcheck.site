f099c8ada7453c9d016b7a54ffe271129fb415ed
tkachuk <z.tkachuk@nodeart.io>
2022-11-30 00:32:58 +0200

    fix promo

--- src/routes/promo/social-bee/index.svelte
+++ src/routes/promo/social-bee/index.svelte
@@ -32,7 +32,7 @@
       </p>
     </div>
     <div class="col right">
-      <img src={sassb} />
+      <img src={sassb} alt="sassb" />
       <div class="field">
         <input
           class="input input-verify"
@@ -58,10 +58,16 @@
   main {
     --heading-color: #ffffff;
     --promo-color: #9747ff;
-    padding: var(--size-30) 5.625rem;
+    padding: var(--size-30);
     min-height: 31.25rem;
 
+    @media all and (min-width: 992px) {
+      padding: var(--size-30) 5.625rem;
+    }
+
     .row {
+      z-index: 1;
+      position: relative;
       display: flex;
       align-items: center;
       flex-flow: column wrap;
@@ -72,13 +78,14 @@
       }
 
       .col {
-        max-width: 40%;
+        max-width: 100%;
         width: auto;
         flex: 1 0 auto;
         text-align: center;
         margin-bottom: var(--size-30);
 
         @media all and (min-width: 992px) {
+          max-width: 40%;
           margin-bottom: 0;
           text-align: left;
         }
@@ -91,7 +98,67 @@
           .field {
             display: flex;
             align-items: center;
-            justify-content: flex-end;
+            justify-content: center;
+            flex-flow: column wrap;
+
+            @media all and (min-width: 992px) {
+              justify-content: flex-end;
+              flex-flow: row nowrap;
+            }
+
+            .input-verify {
+              position: relative;
+              min-width: 12rem;
+              margin-bottom: var(--size-30);
+              padding: var(--size-20) var(--size-46) var(--size-18) var(--size-16);
+              background-color: var(--dark-01);
+              border-color: var(--transparent);
+              border-radius: 0.25rem;
+
+              @media all and (min-width: 992px) {
+                margin-bottom: 0;
+              }
+
+              &::placeholder {
+                color: var(--dark-02);
+                font-size: var(--size-16);
+                font-style: italic;
+                letter-spacing: var(--letter-spacing);
+              }
+
+              &:focus {
+                font-weight: var(--weight-500);
+                background-color: var(--dark-05);
+                box-shadow: 0 0 0 var(--size-1) var(--primary-white) inset;
+              }
+            }
+
+            .btn-verify-email {
+              top: var(--size-8);
+              right: var(--size-6);
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              min-width: 10rem;
+              margin-left: 0;
+              height: 3.25rem;
+              background-color: var(--color-progress);
+              border: none;
+              border-radius: var(--size-6);
+              transition: 0.35s;
+              color: var(--primary-white);
+              font-weight: var(--weight-700);
+              font-size: var(--size-14);
+              text-transform: uppercase;
+
+              @media all and (min-width: 600px) {
+                margin-left: 1rem;
+              }
+
+              &:hover {
+                transform: translateX(var(--size-2));
+              }
+            }
           }
         }
       }
@@ -99,56 +166,16 @@
   }
 
   img {
-    width: 50%;
-    min-width: 18.75rem;
+    width: 100%;
+    height: auto;
+
+    @media all and (min-width: 600px) {
+      width: 50%;
+      min-width: 18.75rem;
+    }
   }
 
   span {
     color: var(--promo-color);
-  }
-
-  .input-verify {
-    position: relative;
-    margin-bottom: 0;
-    padding: var(--size-20) var(--size-46) var(--size-18) var(--size-16);
-    background-color: var(--dark-01);
-    border-color: var(--transparent);
-    border-radius: 0.25rem;
-
-    &::placeholder {
-      color: var(--dark-02);
-      font-size: var(--size-16);
-      font-style: italic;
-      letter-spacing: var(--letter-spacing);
-    }
-
-    &:focus {
-      font-weight: var(--weight-500);
-      background-color: var(--dark-05);
-      box-shadow: 0 0 0 var(--size-1) var(--primary-white) inset;
-    }
-  }
-
-  .btn-verify-email {
-    top: var(--size-8);
-    right: var(--size-6);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-width: 10rem;
-    margin-left: 1rem;
-    height: 3.25rem;
-    background-color: var(--color-progress);
-    border: none;
-    border-radius: var(--size-6);
-    transition: 0.35s;
-    color: var(--primary-white);
-    font-weight: var(--weight-700);
-    font-size: var(--size-14);
-    text-transform: uppercase;
-
-    &:hover {
-      transform: translateX(var(--size-2));
-    }
   }
 </style>
