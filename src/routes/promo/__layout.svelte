b9f630371993318d4352cd14353c24b32291dd3a
Konstantin Nosov <nosov@nodeart.io>
2022-11-30 13:13:10 +0100

    noindex for promotion pages

---
+++ src/routes/promo/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Seo from "$lib/Seo/index.svelte";
+</script>
+
+<Seo
+  title="MailCheck Promotion"
+  noindex={true}
+  description={'MailCheck promotion'}
+  canonical="/"
+/>
+<slot />
+
