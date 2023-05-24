edac0b6ed6d6d1f6eed322418021d2ca82752c61
tkachuk <z.tkachuk@nodeart.io>
2023-05-24 16:07:01 +0300

    add new point to faq component

--- src/lib/Faq/index.svelte
+++ src/lib/Faq/index.svelte
@@ -13,8 +13,7 @@
   on:enter={(event) => {
     const { inView } = event.detail;
     intersecting = inView;
-  }}
->
+  }}>
   <div class="wrapper-questions">
     <h3 class="title-small">FAQ</h3>
 
@@ -89,6 +88,60 @@
               AGENCY, ENTERPRISE, LEGENDARY) so you are able to verify more emails than you have
               according to your plan, to receive more detailed information, please contact our
               support team
+            </p>
+          </div>
+        </div>
+
+        <div class="tab">
+          <input class="input-radio" type="checkbox" name="acc-tabs" />
+          <span class="tab-label"
+            >Why Mailcheck.co is better than other email validation tools?</span>
+          <div class="tab-content">
+            <p class="tab-text">
+              When it comes to email verification services, there are many popular options. However,
+              if you are looking for a service with recurring plans, Mailcheck.co is the better
+              choice.
+            </p>
+            <p>Here's why:</p>
+            <ol>
+              <li>
+                <strong>Affordable Recurring Plans:</strong> Mailcheck.co offers affordable recurring
+                plans on a monthly or yearly basis, making it easier for businesses to budget for email
+                verification services. With “other email validation tools”, you have to pay for each
+                verification separately, which can be inconvenient if you need regular email verification
+                services.
+              </li>
+              <li>
+                <strong>Wide Range of Features:</strong> Mailcheck.co offers a wide range of features,
+                including disposable email detection, syntax checking, and domain validation, making
+                it a comprehensive and versatile email verification tool. “other email validation tools”
+                offers more advanced features, but for most businesses, the features offered by Mailcheck.co
+                are sufficient.
+              </li>
+              <li>
+                <strong>User-Friendly Interface:</strong> Mailcheck.co's user interface is intuitive
+                and easy to navigate, making it simple for users to upload their email lists and get
+                started with email verification. “Other email validation tools” can be more challenging
+                to use, particularly for users who are new to email verification services.
+              </li>
+              <li>
+                <strong>Excellent Customer Support:</strong> Mailcheck.co provides exceptional customer
+                support, including email and live chat support, as well as a knowledge base that can
+                help you troubleshoot any issues you may encounter. “Other email validation tools” also
+                offers customer support, but Mailcheck.co's support is particularly well-regarded.
+              </li>
+              <li>
+                <strong>Highly Accurate Results:</strong> Both “Another email validation tool” and Mailcheck.co
+                provide highly accurate email verification results. While “other email validation tools”
+                is known for its accuracy, Mailcheck.co also has an excellent reputation for providing
+                accurate verification results.
+              </li>
+            </ol>
+            <p>
+              In summary, if you are looking for an email verification service with recurring plans,
+              Mailcheck.co is the better choice. Its affordable pricing, a wide range of features,
+              user-friendly interface, excellent customer support, and highly accurate results make
+              it a perfect option for businesses looking to keep their email list clean and valid.
             </p>
           </div>
         </div>
