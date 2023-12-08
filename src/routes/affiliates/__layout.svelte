16ec5212a5edf25f5a7a37f743c86860ff5d9b27
Vasiliy Manilo <v.manilo@do-it.co>
2023-12-08 12:18:02 +0200

    feat(MCH-683): remove unused Affiliates layout parts

--- src/routes/affiliates/__layout.svelte
+++ src/routes/affiliates/__layout.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
   import ContactUs from '$lib/ContactUs/index.svelte';
-  import Seo from '$lib/Seo/index.svelte';
-  import { websiteSchema } from '$utils/json-ld';
-
-  const title = 'Mailcheck affiliate program';
-  const desc = title + '. Earn the highest commissions on referrals.';
 </script>
-
-<Seo {desc} {title} schemas={[websiteSchema]} />
 
 <slot />
 
